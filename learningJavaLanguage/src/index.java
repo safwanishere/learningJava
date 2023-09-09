2cf703781dddcd06501a9179a1b305509ec7ce75
safwan <mohammedsafwan261205@gmail.com>
2023-09-09 22:50:05 +0530

    if statement

--- learningJavaLanguage/src/index.java
+++ learningJavaLanguage/src/index.java
@@ -2,5 +2,9 @@ public class Index {
     public static void  main(String[] args){
         System.out.println("Hello World");
         System.out.println("\"Hello \nworld\"");
+
+        if (true){
+            System.out.println("condition true");
+        }
     }
 }
