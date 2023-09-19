d3d21ffe2c5d3c2e91e5ab4b5a9e344189016b62
safwan <mohammedsafwan261205@gmail.com>
2023-09-19 15:53:35 +0530

    variable declaration and print statement variations

--- learningJavaLanguage/src/index.java
+++ learningJavaLanguage/src/index.java
@@ -1,10 +1,12 @@
 public class Index {
     public static void  main(String[] args){
-        System.out.println("**********");
-		System.out.println("*        *");
-		System.out.println("*        *");
-		System.out.println("*        *");
-		System.out.println("*        *");
-		System.out.println("**********");
+		byte age = 75;
+		int mobilenumber = 98420;
+		int pincode = 500017;
+		long aadharnumber = 427104209978L;
+		int debitCardNumber = 56781234;
+
+
+		System.out.println(age+"\n"+mobilenumber+"\n"+pincode+"\n"+aadharnumber+"\n"+debitCardNumber);
     }
 }
