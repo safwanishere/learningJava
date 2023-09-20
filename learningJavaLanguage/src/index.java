f6fe4a74bd6578226955ffc39a6603f8458b0f7b
safwan <mohammedsafwan261205@gmail.com>
2023-09-20 19:22:02 +0530

    lab class day 2

--- learningJavaLanguage/src/index.java
+++ learningJavaLanguage/src/index.java
@@ -1,12 +1,56 @@
 public class Index {
     public static void  main(String[] args){
-		byte age = 75;
-		int mobilenumber = 98420;
-		int pincode = 500017;
-		long aadharnumber = 427104209978L;
-		int debitCardNumber = 56781234;
+		// byte age = 75;
+		// int mobilenumber = 98420;
+		// int pincode = 500017;
+		// long aadharnumber = 427104209978L;
+		// int debitCardNumber = 56781234;
 
 
-		System.out.println(age+"\n"+mobilenumber+"\n"+pincode+"\n"+aadharnumber+"\n"+debitCardNumber);
+		// System.out.println(age+"\n"+mobilenumber+"\n"+pincode+"\n"+aadharnumber+"\n"+debitCardNumber);
+
+		// float samsCandies = 75f;
+		// float angelsCandies = 0f;
+		
+		// angelsCandies += samsCandies/2;
+		// samsCandies -= samsCandies/2;
+		
+		// samsCandies += angelsCandies/2;
+		// angelsCandies -= angelsCandies/2;
+		
+		// System.out.println("Candy numbers");
+		// System.out.println("sam's candies: " + samsCandies);
+		// System.out.println("Angel's candies: " + angelsCandies);
+
+		// byte s1 = 80;
+		// byte s2 = 75;
+		// byte s3 = 98;
+		// byte s4 = 67;
+		// byte s5 = 50;
+
+		// float average = (s1 + s2 + s3 + s4 + s5) / 5f;
+		// float percentage = (s1 + s2 + s3 + s4 + s5) * 100/500f;
+
+		// System.out.println("Average: " + average);
+		// System.out.println("Percentage: " + percentage + "\n\n");
+
+		// byte x = 5;
+		// byte y = ++x;
+		// System.out.println(x + " " + y);
+
+		// if (false){
+		// 	System.out.println("\n");
+		// 	System.out.println("      **      **      ");
+		// 	System.out.println("   *******  *******    ");
+		// 	System.out.println("   ****************   ");
+		// 	System.out.println("    **************    ");
+		// 	System.out.println("      **********      ");
+		// 	System.out.println("        *******        ");
+		// 	System.out.println("           *          ");
+		// } else if (true){
+		// 	System.out.println("check");
+		// } else{
+		// 	System.out.println("not confirmed");
+		// }
     }
 }
