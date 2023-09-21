69354d40916fe53af1feec5b75c7a7637fa28202
safwan <mohammedsafwan261205@gmail.com>
2023-09-21 15:58:31 +0530

    conditonal statements

--- learningJavaLanguage/src/Daythree.java
+++ learningJavaLanguage/src/Daythree.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class Daythree {
     public static void main(String args[]){
         // int a=6, b=4, c=2;
@@ -13,6 +14,42 @@ public class Daythree {
         // System.out.println(b+c^(a&b));  
         // System.out.println(a+b-c*(a*b*c));
 
+        // Scanner scan = new Scanner(System.in);
+        // int a = scan.nextInt();
+        // int b = scan.nextInt();
+        // int c = scan.nextInt();
+        // if (a>b && a>c){
+        //     System.out.println(a + " A is greater than B and C");
+        // }
+        // else if (b>c && b>a){
+        //     System.out.println(b + " B is greater than A and C");
+        // }
+        // else if (c>a && c>b){
+        //     System.out.println(c + " C is greater than A and B");
+        // }
+        
+        // Scanner scan = new Scanner(System.in);
+        // System.out.print("enter your number: ");
+        // int num = scan.nextInt();
+        // if (num > 0){
+        //     System.out.println(num + " is a positive number");
+        // }
+        // else if (num < 0){
+        //     System.out.println(num + " is a negative number");
+        // }
+        // else{
+        //     System.out.println("given number is zero");
+        // }
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("enter your number: ");
+        int num = scan.nextInt();
+        if (num%2 == 0){
+            System.out.println(num + " is an even number");
+        }
+        else{
+            System.out.println(num + " is an odd number");
+        }
         
     }
 }
