25a16b5c39941ba28fe84543d797c93a4d7924b3
safwan <mohammedsafwan261205@gmail.com>
2023-09-25 23:30:09 +0530

    day seven

---
+++ learningJavaLanguage/src/DaySeven.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class DaySeven {
+    public static void main(String args[]){
+        int [] arr={1,7,10,56,23};
+        int i, minele=arr[0];
+        for(i=0; i<5; i++){
+            if (arr) []
+        }
+
+        
+    }
+
+    public static void hello(){
+        System.out.println("hello world");
+    }
+}
