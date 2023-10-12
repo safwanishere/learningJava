2062100e7a1011435984f7672f0f3729d572b44a
safwan <mohammedsafwan261205@gmail.com>
2023-10-12 11:28:31 +0530

    java programming class

---
+++ learningJavaLanguage/src/AddTwoNumbers.java
@@ -0,0 +1,9 @@
+public class AddTwoNumbers {
+    public static void main(String args[]){
+        byte n1 = 8;
+        byte n2 = 15;
+        byte n3 = 25;
+        int n = n1+n2+n3;
+        System.out.println(n);
+    }
+}
