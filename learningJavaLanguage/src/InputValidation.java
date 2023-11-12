db0ca1cfa2d2200db6bd7e88110ae30d3c3df36f
safwan <mohammedsafwan261205@gmail.com>
2023-11-12 23:27:11 +0530

    inputValidation program

--- learningJavaLanguage/src/InputValidation.java
+++ learningJavaLanguage/src/InputValidation.java
@@ -3,12 +3,16 @@ import java.util.Scanner;
 public class InputValidation {
     public static void main(String args[]){
         Scanner scan = new Scanner(System.in);
-        System.out.print("Enter a number between 0-10 or 90-100: ");
-        int input = scan.nextInt();
 
         while (true) {
-            if (input == -1){
-                
+            System.out.print("Enter a number between 0-10 or 90-100: ");
+            int input = scan.nextInt();
+            if ((input>=0 && input<=10) || (input>=90 && input<=100)){
+                System.out.println("You have entered: "+input);
+                break;
+            }
+            else{
+                System.out.println("Invalid input, try again...");
             }
         }
     }
