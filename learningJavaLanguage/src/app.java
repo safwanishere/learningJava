dc5804b34729357b3c591723f9a9f5393c7fd078
safwan <mohammedsafwan261205@gmail.com>
2023-11-21 20:30:04 +0530

    overloading and overriding

--- learningJavaLanguage/src/app.java
+++ learningJavaLanguage/src/app.java
@@ -1,9 +1,30 @@
 public class app {
     public static void main(String args[]){
-        int i = 0;
-        for (i=10; i<20; i++);
-        {
-            System.out.println(i);
-        }
+        Math math = new Math();
+
+        System.out.println(math.add(2,2));
+        System.out.println(math.add(1,2,2));
+
+        Numbers numbers = new Numbers();
+
+        System.out.println(numbers.add(1,2));
+    }
+}
+
+class Math {
+    public int add(int x, int y){
+        return x + y;
+    }
+    public int add(int x, int y, int z){
+        return x + y + z;
+    }
+}
+
+class Numbers extends Math{
+    public int add(int x, int y, int z, int a){
+        return x + y + z + a;
+    }
+    public int add(int x, int y){
+        return x + x;
     }
 }
