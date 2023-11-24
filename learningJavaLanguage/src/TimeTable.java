497df4a6ee6ab87f0ea420e45711b02f6c543698
safwan <mohammedsafwan261205@gmail.com>
2023-11-24 11:05:18 +0530

    finetuning exercise 5

--- learningJavaLanguage/src/TimeTable.java
+++ learningJavaLanguage/src/TimeTable.java
@@ -7,7 +7,18 @@ public class TimeTable {
         int size = scan.nextInt();
         scan.close();
 
+        System.out.print("*\t|\t");
+        for (int i = 1; i <= size; i++){
+            System.out.print(i+"\t");
+        }
+        System.out.println();
+        for (int i = 1; i <= size; i++){
+            System.out.print("---------");
+        }
+        System.out.println();
+
         for (int row = 1; row <= size; row++){
+            System.out.print(row + "\t|\t");
             for (int col = 1; col <= size; col++){
                 System.out.print(col*row + "\t");
             }
