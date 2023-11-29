4b90aa3e93daf504828ffd31e15bbc563ab9db08
safwan <mohammedsafwan261205@gmail.com>
2023-11-29 23:04:14 +0530

    created 6th exercise sub folder

--- learningJavaLanguage/src/6th Exercise/CapricornNumber.java
+++ learningJavaLanguage/src/6th Exercise/CapricornNumber.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.lang.Math;
 
 public class CapricornNumber {
     public static void main(String args[]){
@@ -7,17 +8,16 @@ public class CapricornNumber {
         int num = scan.nextInt();
         scan.close();
 
-        int squareOfNum = num*num, count = 0, addition;
+        int squareOfNum = num*num, count = 0;
         boolean check = true;
 
         for (int i = squareOfNum; i>0; i/=10){
             count += 1;
         }
-        System.out.println(count);
+
         for (int i = 1; i<count; i++){
-            addition = squareOfNum%(10^i) + squareOfNum%(10^(count-i));
-            System.out.println(addition);
-            if (addition == num){
+            
+            if ( == num){
                 System.out.println(num + " is a Capricorn/Kaprekar number");
             }
             else {
---
+++ learningJavaLanguage/src/class Cstr.java
@@ -0,0 +1,35 @@
+class Cstr
+{ 
+    int a,b;
+    int sum;
+    cstr()
+    {
+        a=10;
+        b=20;
+    }
+    cstr(int x)
+    {
+        a=x;
+        b=x;
+    }
+    cstr(int a,int y)
+    {
+        a=x;
+        b=y;
+    }
+
+void Add
+{
+    sum=a+b;
+    System.out.println(sum);
+}
+public static void main(String[]args)
+{
+cstr obj1=new cstr();
+cstr obj2=new cstr(15);
+cstr obj3=new cstr(1,2);
+obj1.Add();
+obj2.Add();
+obj3.Add();
+}
+}
