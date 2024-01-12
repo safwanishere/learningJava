663775d4e6e86d52a2b3f37bde08cfcb1cbd70d6
safwan <mohammedsafwan261205@gmail.com>
2024-01-12 22:44:57 +0530

    numberGuess game

--- learningJavaLanguage/src/NumberGuess.java
+++ learningJavaLanguage/src/NumberGuess.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class NumberGuess{
-    public static void main(String args){
-        
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in);
+        final int secretNumber = (int)(Math.random()*100);
+        int count = 0;
+        System.out.println("Key in your guess: ");
+
+        while (true){
+            int guess = scan.nextInt();
+            count++;
+
+            if (guess == secretNumber){
+                System.out.print("you got it in " + count + " trials!");
+                break;
+            }
+            else if (guess > secretNumber){
+                System.out.println("Try lower");
+            }
+            else if (guess < secretNumber){
+                System.out.println("Try higher");
+            }
+        }
     }
 }
