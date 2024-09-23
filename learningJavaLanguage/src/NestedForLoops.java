8db38309f67ec1093ba87df9bf529b5a06faba63
safwan <mohammedsafwan261205@gmail.com>
2024-09-23 22:21:46 +0530

    nested for loops n shi

---
+++ learningJavaLanguage/src/NestedForLoops.java
@@ -0,0 +1,13 @@
+public class NestedForLoops{
+    public static void main(String[] args){
+        int count =0;
+        for(int i = 0 ; i < 10 ; i+=2){
+            //for(int j = 5 ; j > i ; j--){
+                //System.out.print("# ");
+          //  }
+          count += i;
+           
+        } System.out.println(count);
+
+    }
+}
